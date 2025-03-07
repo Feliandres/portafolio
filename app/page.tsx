829183cad5fe574097745f665b3034a2d35033f0
Feliandres <85601974+Feliandres@users.noreply.github.com>
2025-03-07 15:53:41 -0500

    10-modularize-sections-in-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,6 @@
-import { About } from "@/components/About";
-import { Contact } from "@/components/Contact";
-import { Experience } from "@/components/Experience";
-import { Footer } from "@/components/Footer";
-import { Hero } from "@/components/Hero";
-import { KeyMetrics } from "@/components/KeyMetrics";
-import { LogoAnimation } from "@/components/LogoAnimation";
+import { sections } from "@/lib/constants";
 import { Navbar } from "@/components/Navbar";
-import { Portfolio } from "@/components/Portfolio";
-import { Services } from "@/components/Services";
-import { Skills } from "@/components/Skills";
-
-const sections = [
-    { id: "hero", component: <Hero /> },
-    { id: "skills", component: <Skills /> },
-    { id: "logo-animation", component: <LogoAnimation /> },
-    { id: "about", component: <About /> },
-    { id: "experience", component: <Experience /> },
-    { id: "portfolio", component: <Portfolio /> },
-    { id: "key-metrics", component: <KeyMetrics /> },
-    { id: "services", component: <Services /> },
-    { id: "contact", component: <Contact /> },
-];
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
     return (
@@ -28,9 +8,9 @@ export default function Home() {
             <Navbar />
 
             <main className="text-white space-y-16 px-4 sm:px-8 lg:px-16 xl:px-32">
-                {sections.map(({ id, component }) => (
+                {sections.map(({ id, component: Component }) => (
                     <section key={id} id={id} className="py-16">
-                        {component}
+                        <Component />  {/* Se renderiza el componente */}
                     </section>
                 ))}
             </main>
